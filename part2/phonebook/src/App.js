bf445a1633a8f6c8c1ca64aae4954ca4326b75ae
ClaraCrocodilo <vinicius.ferreira1618@gmail.com>
2021-09-02 13:30:50 -0300

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,10 +12,14 @@ const App = () => {
 
     const addName = (event) => {
         event.preventDefault();
-        const newPerson = {
-            name: newName
+        if (persons.map(person => person.name).includes(newName)) {
+            window.alert(`${newName} is already added to phonebook`);
+        } else {
+            const newPerson = {
+                name: newName
+            };
+            setPersons(persons.concat(newPerson));
         };
-        setPersons(persons.concat(newPerson));
     };
 
     return (
