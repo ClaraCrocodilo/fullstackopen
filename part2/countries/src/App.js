0f7d04e66611b34e765ea6b8eb7015581953fa67
ClaraCrocodilo <vinicius.ferreira1618@gmail.com>
2021-09-03 10:51:59 -0300

    2.13*: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Countries = ({ countries }) => {
+const Country = ({ country }) => (
+    <div>
+        <h1>{country.name}</h1>
+        capital {country.capital} <br />
+        population {country.population} <br />
+        <h2>languages</h2>
+        <ul>
+            {country.languages.map(lang => (
+                <li key={lang.iso639_1}>
+                    {lang.name}
+                </li>
+            ))}
+        </ul>
+        <img
+            src={country.flag}
+            alt={`${country.name} flag`}
+            width="160px"
+        />
+    </div>
+);
+
+const Countries = ({ countries, shownCountries, setShownCountries,
+    handleShownCountriesChange }) => {
+
+
     if (countries.length > 10) {
         return (
             <div>
@@ -11,32 +35,18 @@ const Countries = ({ countries }) => {
     } else if (countries.length === 0) {
         return <div></div>
     } else if (countries.length === 1) {
-        const country = countries[0];
         return (
-            <div>
-                <h1>{country.name}</h1>
-                capital {country.capital} <br />
-                population {country.population} <br />
-                <h2>languages</h2>
-                <ul>
-                    {country.languages.map(lang => (
-                        <li key={lang.iso639_1}>
-                            {lang.name}
-                        </li>
-                    ))}
-                </ul>
-                <img
-                    src={country.flag}
-                    alt={`${country.name} flag`}
-                    width="160px"
-                />
-            </div>
+            <Country country={countries[0]}/>
         );
     } else {
         return (
             countries.map(country => (
                 <div key={country.alpha3Code}>
                     {country.name}
+                    <button onClick={handleShownCountriesChange(country)}>
+                        {(shownCountries[country.name]) ? "hide" : "show"}
+                    </button>
+                    {shownCountries[country.name] && <Country country={country}/>}
                 </div>
             ))
         );
@@ -46,10 +56,17 @@ const Countries = ({ countries }) => {
 const App = () => {
     const [search, setSearch] = useState('');
     const [countries, setCountries] = useState([]);
+    const [shownCountries, setShownCountries] = useState({});
 
     const handleSearchChange = (event) => {
         event.preventDefault();
         setSearch(event.target.value);
+    };
+    const handleShownCountriesChange = country => () => {
+        console.log(country, shownCountries)
+        setShownCountries({...shownCountries,
+            [country.name]: !shownCountries[country.name]
+        });
     };
     
     useEffect(() => {
@@ -57,6 +74,9 @@ const App = () => {
         axios.get(`https://restcountries.eu/rest/v2/name/${search}`)
             .then(response => {
                 setCountries(response.data);
+                setShownCountries(Object.fromEntries(response.data.map(country => {
+                    return [country.name, false]
+                })));
             })
             .catch(err => setCountries([]));
     }, [search]);
@@ -67,7 +87,12 @@ const App = () => {
                 value={search}
                 onChange={handleSearchChange}
             />
-            <Countries countries={countries}/>
+            <Countries 
+                countries={countries}
+                shownCountries={shownCountries}
+                setShownCountries={setShownCountries}
+                handleShownCountriesChange={handleShownCountriesChange}
+            />
         </div>
     );
 };
