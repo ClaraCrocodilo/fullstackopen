2091af2d27c71986326f4e15821d3b029a6ed2dd
ClaraCrocodilo <vinicius.ferreira1618@gmail.com>
2021-09-04 18:10:50 -0300

    2.17: Phonebook step9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,50 +1,60 @@
-    import React, { useState, useEffect } from 'react';
-    import axios from 'axios';
-    import numberService from './services/numbers';
+import React, { useState, useEffect } from 'react';
+import numberService from './services/numbers';
 
-    const Filter = ({ filter, handleFilterChange }) => (
-        <form>
-            <div>
-                filter shown with <input
-                    value={filter}
-                    onChange={handleFilterChange}
-                />
-            </div>
-        </form>
-    );
+const Filter = ({ filter, handleFilterChange }) => (
+    <form>
+        <div>
+            filter shown with <input
+                value={filter}
+                onChange={handleFilterChange}
+            />
+        </div>
+    </form>
+);
 
-    const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => (
-        <form onSubmit={addPerson}>
-            <div>
-                name: <input
-                    value={newName}
-                    onChange={handleNameChange}
-                />
-            </div>
-            <div>
-                number: <input
-                    value={newNumber}
-                    onChange={handleNumberChange}
-                />
-            </div>
-            <div>
-                <button type="submit">add</button>
-            </div>
-        </form>
-    );
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => (
+    <form onSubmit={addPerson}>
+        <div>
+            name: <input
+                value={newName}
+                onChange={handleNameChange}
+            />
+        </div>
+        <div>
+            number: <input
+                value={newNumber}
+                onChange={handleNumberChange}
+            />
+        </div>
+        <div>
+            <button type="submit">add</button>
+        </div>
+    </form>
+);
 
-    const Persons = ({ persons, filter }) => (
-        <>
-        {persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-                .map(person => <div key={person.name}>{person.name} {person.number}</div>)}
-        </>
-    );
+const Persons = ({ persons, filter, deleteNumber }) => (
+    <>
+        {persons.filter(person => person.name.toLowerCase()
+            .includes(filter.toLowerCase()))
+                .map(person => {
+                    return (
+                        <div key={person.name}>
+                            {person.name} {person.number}
+                            <button onClick={deleteNumber(person)}>
+                                delete
+                            </button>
+                        </div>
+                    );
+                })
+        }
+    </>
+);
 
-    const App = () => {
-        const [persons, setPersons] = useState([]);
-        const [newName, setNewName] = useState('');
-        const [newNumber, setNewNumber] = useState('');
-        const [filter, setFilter] = useState('');
+const App = () => {
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [filter, setFilter] = useState('');
 
     const handleNameChange = (event) => {
         setNewName(event.target.value);
@@ -76,11 +86,20 @@
             };
             numberService
                 .create(newPerson)
-                .then(returnedNote => {
-                    setPersons(persons.concat(newPerson));
+                .then(returnedPerson => {
+                    setPersons(persons.concat(returnedPerson));
                 });
         };
-    };
+};
+
+    const deleteNumber = (person) => () => {
+        if (window.confirm(`Delete ${person.name}?`)) {
+            numberService.deleteNumber(person.id)
+                .then(returnedData => {
+                    setPersons(persons.filter(p => p.id !== person.id))
+                });
+        };
+};
 
     return (
         <div>
@@ -100,7 +119,7 @@
 
             <h3>Numbers</h3>
 
-            <Persons persons={persons} filter={filter}/>
+            <Persons persons={persons} filter={filter} deleteNumber={deleteNumber}/>
         </div>
     )
 };
