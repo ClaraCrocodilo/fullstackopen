db0118627f7549c495ce22b1a0954f72f5d3b52f
ClaraCrocodilo <vinicius.ferreira1618@gmail.com>
2021-10-14 10:24:24 -0300

    3.18*: Phonebook database step6

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -58,9 +58,10 @@ const generateId = () => {
 generateId()
 
 app.get("/info", (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>`
-    + `<p> ${new Date()} </p>`
-    );
+    Person.countDocuments().then(count => {
+        response.send(`<p>Phonebook has info for ${count} people</p>`
+            + `<p> ${new Date()} </p>`);
+    }).catch(error => next(error));
 });
 
 app.get("/api/persons", (request, response, next) => {
