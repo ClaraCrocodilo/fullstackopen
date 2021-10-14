16e640d2ba4131390ae6cd1df473a63d835bd9bd
ClaraCrocodilo <vinicius.ferreira1618@gmail.com>
2021-10-14 09:33:15 -0300

    3.16: Phonebook database, step4

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -63,23 +63,23 @@ app.get("/info", (request, response) => {
     );
 });
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
     Person.find({}).then(people => {
         response.json(people);
-    });
+    }).catch(error => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
     Person.findById(request.params.id).then(people => {
         response.json(people);
-    });
+    }).catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end();
-        });
+        }).catch(error => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -105,9 +105,21 @@ app.post("/api/persons", (request, response) => {
     });
 
     person.save().then(savedPerson => {
-        response.json(savedPerson);
+        response.json(savedPerson).catch(error => next(error));
     });
 });
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    } else {
+        next(error);
+    };
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
