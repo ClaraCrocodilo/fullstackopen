4f2adf8cdc8d9a9ec4d0ee6f79291afbf9914e21
ClaraCrocodilo <vinicius.ferreira1618@gmail.com>
2022-05-23 18:28:20 -0300

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    const reducer = (favorite, blog) => {
+        return blog.likes > favorite.likes
+            ? blog
+            : favorite;
+    };
+
+    return blogs.reduce(reducer);
+};
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
